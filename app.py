bc99d3ae5ec14c370aeb29e5f02b7b7550a5a462
Eddie Shin <eshinhw@gmail.com>
2023-07-29 14:41:39 -0400

    implemented the homepage layout

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from dash import Dash, html, dcc, callback, Output, Input, dash_table
+import plotly.express as px
+import pandas as pd
+import dash_bootstrap_components as dbc
 
-app = Flask(__name__)
+from components.navbar import navbar
 
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+app.layout = html.Div(
+    [
+        navbar,
+        dcc.Graph(id="total-perf-graph"),
+        html.Div([dash_table.DataTable(id="total-perf-table")]),
+    ]
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
